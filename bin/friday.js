7f3415d87bc89ac36387d80fd7b60ddbfd1d8345
Sridattasai18 <kaligotlasridattasai18@gmail.com>
2026-04-10 02:45:45 +0530

    Fix .jsx file extension issue for global npm install

--- bin/friday.js
+++ bin/friday.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
-import('../src/app.jsx').catch((err) => {
-  console.error('Failed to start F.R.I.D.A.Y:', err);
-  process.exit(1);
-});
+import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { cwd } from 'process';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const appPath = join(__dirname, '../src/app.jsx');
+const proc = spawn('npx', ['tsx', appPath], { stdio: 'inherit', shell: true, cwd: __dirname });
+proc.on('exit', (code) => process.exit(code ?? 0));
